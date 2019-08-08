3a9c09a984a7ca3a64b9dc628a9a6ec70bcb274b
OTree <kevin10410@gmail.com>
2019-08-08 17:38:04 +0800

    feat(HelloWorld): add increase & decrease button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,12 @@
   .hello
     h1 {{ msg }}
     p Current value {{ value }}
+    button.hello__btn(
+      @click="increase"
+    ) +
+    button.hello__btn(
+      @click="decrease"
+    ) -
 </template>
 
 <script lang="ts">
@@ -12,9 +18,34 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 
   @Prop() private value!: number;
+
+  increase() {
+    this.$emit('increase');
+  }
+
+  decrease() {
+    this.$emit('decrease');
+  }
 }
 </script>
 
 
 <style scoped lang="scss">
+.hello {
+  &__btn {
+    width: 40px;
+    height: 30px;
+    outline: none;
+    border: 1px solid black;
+    border-radius: 5px;
+    margin: 5px;
+    font-size: 20px;
+    &:hover {
+      background: #e3e3e3;
+    }
+    &:active {
+      background: #d1d1d1;
+    }
+  }
+}
 </style>
